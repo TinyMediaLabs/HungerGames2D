9d08807762fb771839832fb5272576fb80b9734a
Augustas Noah Grebliauskas <noah.grebliauskas@gmail.com>
2015-06-08 19:27:05 +0300

    Working Player API

---
+++ Assets/Scripts/Player/PlayerFeetMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFeetMovement : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float inputV = Input.GetAxis("Vertical");
+        if ((inputV > 0) || (inputV < 0))
+        {
+            anim.SetBool("Forward", true);
+        }
+        else
+        {
+            anim.SetBool("Forward", false);
+        }
+
+        float inputH = Input.GetAxis("Horizontal");
+        if(inputH > 0)
+        {
+            anim.SetBool("Right", true);
+        }
+        else if (inputH < 0)
+        {
+            anim.SetBool("Left", true);
+        }
+        else
+        {
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+        float inputV = Input.GetAxis("Vertical");
+        if(inputV > 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * inputV);
+
+            anim.SetBool("ForwardMovement", true);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed * inputV);
+            anim.SetBool("ForwardMovement", false);
+        }
+
+        float inputH = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * speed * inputH);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("MeleeAttackTrigger");
+        }
+    }
+}
