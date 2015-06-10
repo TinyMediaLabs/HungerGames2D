6911fda9f13852e99ca9e856664d51c2d62ad79c
Augustas Noah Grebliauskas <noah.grebliauskas@gmail.com>
2015-06-10 19:47:21 +0300

    Started working on Multiplayer

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Connect();
+	}
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings("V.Alpha.0.0.1");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+    
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        SpawnMyPlayer();
+    }
+
+    void SpawnMyPlayer()
+    {
+        GameObject myPlayerObject = (GameObject) PhotonNetwork.Instantiate("TopDownPlayerController", Vector3.zero, Quaternion.identity, 0);
+
+        myPlayerObject.transform.FindChild("PlayerCamera").gameObject.SetActive(true);
+
+        GameObject myPlayerBody = myPlayerObject.transform.Find("PlayerObject").gameObject;
+        GameObject myPlayerFeet = myPlayerBody.transform.Find("Feet").gameObject;
+
+        ((MonoBehaviour)myPlayerBody.GetComponent("PlayerMovement")).enabled = true;
+        ((MonoBehaviour)myPlayerFeet.GetComponent("PlayerFeetMovement")).enabled = true;
+    }
+}
---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+    Vector3 realPosition = Vector3.zero;
+    Quaternion realRotation = Quaternion.identity;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    if(photonView.isMine)
+        {
+
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+        }
+	}
+
+    void PhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            realPosition = (Vector3) stream.ReceiveNext();
+            realRotation = (Quaternion) stream.ReceiveNext();
+        }
+    }
+}
