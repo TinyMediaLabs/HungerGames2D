28e16ae2b3cbfbd9de388a1368b2fa94b0d57c84
Augustas Noah Grebliauskas <noah.grebliauskas@gmail.com>
2015-06-11 00:08:02 +0300

    Began working on a Bow

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class NetworkPlayer : Photon.MonoBehaviour {
 
     Animator bodyAnim;
+    Animator feetAnim;
+
+    GameObject feetObject;
 
     Vector3 realPosition = Vector3.zero;
     Quaternion realRotation = Quaternion.identity;
@@ -11,7 +14,9 @@ public class NetworkPlayer : Photon.MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-	
+        bodyAnim = GetComponent<Animator>();
+        feetObject = gameObject.transform.Find("Feet").gameObject;
+        feetAnim = feetObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +24,6 @@ public class NetworkPlayer : Photon.MonoBehaviour {
     {
 	    if(photonView.isMine)
         {
-            bodyAnim = GetComponent<Animator>();
             if(bodyAnim == null)
             {
                 Debug.Log("bodyAnim = NULL!");
@@ -39,12 +43,18 @@ public class NetworkPlayer : Photon.MonoBehaviour {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
             stream.SendNext(bodyAnim.GetBool("ForwardMovement"));
+            stream.SendNext(feetAnim.GetBool("Forward"));
+            stream.SendNext(feetAnim.GetBool("Left"));
+            stream.SendNext(feetAnim.GetBool("Right"));
         }
         else
         {
             realPosition = (Vector3) stream.ReceiveNext();
             realRotation = (Quaternion) stream.ReceiveNext();
             bodyAnim.SetBool("ForwardMovement", ((bool)stream.ReceiveNext()));
+            feetAnim.SetBool("Forward", ((bool)stream.ReceiveNext()));
+            feetAnim.SetBool("Left", ((bool)stream.ReceiveNext()));
+            feetAnim.SetBool("Right", ((bool)stream.ReceiveNext()));
         }
     }
 }
